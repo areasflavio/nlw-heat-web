e9b9755cca4a54c2de6a55bc5e48c88aec7329bd
Flávio Arêas <vflavio9@gmail.com>
2021-10-26 21:06:19 -0300

    feat: create LoginBox component

---
+++ src/components/LoginBox/index.tsx
@@ -0,0 +1,17 @@
+import { VscGithubInverted } from 'react-icons/vsc';
+
+import styles from './styles.module.scss';
+
+const LoginBox: React.FC = () => {
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href="#" className={styles.signInWithGithub}>
+        <VscGithubInverted size="24" />
+        Entrar com Github
+      </a>
+    </div>
+  );
+};
+
+export { LoginBox };
